5424fdb72979ce39c61c65c630cd5cd798f57d86
cuiyaoDroid <592425690@qq.com>
2015-01-26 12:01:03 +0800

    init

---
+++ src/com/xianzhi/baichengdynamic/TrainingApp.java
@@ -0,0 +1,18 @@
+package com.xianzhi.baichengdynamic;
+
+import com.tingshuo.tool.CrashHandler;
+
+import android.app.Application;
+import android.os.Environment;
+
+public class TrainingApp extends Application{
+	
+	public static final String appPath = Environment
+			.getExternalStorageDirectory() + "/.bc_rail_data/";
+	@Override
+	public void onCreate() {
+		// TODO Auto-generated method stub
+		super.onCreate();
+		CrashHandler.getInstance().init(this);
+	}
+}
